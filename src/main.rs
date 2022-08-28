54ab409ef5307763511ec7f3351c13f05766aae9
Ashneal Powell <pashneal@gmail.com>
2022-08-27 23:39:42 -0400

    Main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+#![no_std]
+use core::arch::asm;
+
+use core::panic::PanicInfo;
+
+#[panic_handler]
+fn panic(_panic: &PanicInfo<'_>) -> ! {
+        loop {}
+}
+
+fn set_reg(){
+    unsafe { 
+        asm!(
+            "ldi r14, 0xffff",
+            "xor r30, r30, r14");
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
 }
